9c06b7d36468cded4efdaf1ce562ababc888bdad
Xavier Lian <xavierliancw@gmail.com>
2016-04-04 20:29:31 -0700

    Pushing updated Developer Resources

---
+++ Baseball-Itinerary-Program/datastructures.cpp
@@ -0,0 +1,7 @@
+#include "datastructures.h"
+
+datastructures::datastructures()
+{
+
+}
+
---
+++ Baseball-Itinerary-Program/datastructures.h
@@ -0,0 +1,15 @@
+#ifndef DATASTRUCTURES_H
+#define DATASTRUCTURES_H
+
+
+class datastructures
+{
+public:
+    datastructures();
+
+signals:
+
+public slots:
+};
+
+#endif // DATASTRUCTURES_H
